418e7b39f607390f11df2ffdb8d7a9293f417183
Dawna Ara <dawnaara@gmail.com>
2016-08-31 22:32:22 -0700

    User sign in/out completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'users/show'
 
   devise_for :users
-root to: "welcome#index"
+
+  resources :users, only: [:show]
+
+  root 'welcome#index'
 end
