760c8b24c9832d6a0d90fabb28b5999c8228f982
Dawna Ara <dawnaara@gmail.com>
2016-09-17 20:18:52 -0700

    assignment 9 completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-	#def markdown(body)
-		#renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+	def markdown(body)
+		renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
 	
-		#Redcarpet::Markdown.new(renderer).render(body).html_safe
-	#end
+		Redcarpet::Markdown.new(renderer).render(body).html_safe
+	end
 
 end
